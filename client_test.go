bb3e686b2770a0d4fd315a75cf923198f0715e7d
EEsign <111329801+EEsign@users.noreply.github.com>
2021-03-02 06:14:12 +0000

    favicon change

--- client_test.go
+++ client_test.go
@@ -39,4 +39,4 @@ func TestNewClient(t *testing.T) {
 
 func TestClient_SubscribePrice(t *testing.T) {
 	ch := make(chan *PriceData, 20)
-	cancel, errC, err := c.SubscribePrice(
+	cancel, errC, err := c.SubscribePrice(pairs
