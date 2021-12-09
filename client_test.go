d97a5f909e79d27d88808041d66df04288e877a7
EEsign <111329801+EEsign@users.noreply.github.com>
2021-12-09 06:37:08 +0000

    docs(api): update docs [skip ci]

--- client_test.go
+++ client_test.go
@@ -120,4 +120,4 @@ func TestClient_GetPairs(t *testing.T) {
 
 func TestClient_CreateOrderByStream(t *testing.T) {
 	req := CreateOrderReq{
-		Pair:              "WBNB-BUSD@
+		Pair:              "WBNB-BUSD@PANCAK
