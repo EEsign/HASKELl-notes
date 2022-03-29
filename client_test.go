f0ae607118de754baa1755c59d7183681378affd
EEsign <111329801+EEsign@users.noreply.github.com>
2022-03-29 11:37:16 +0000

    Restructuring project

--- client_test.go
+++ client_test.go
@@ -137,4 +137,4 @@ func TestClient_CreateOrderByStream(t *testing.T) {
 }
 
 func TestClient_GetOrderResult(t *testing.T) {
-	result, err := c.GetOrderResult(ct
+	result, err := c.GetOrderResult(ctx, 
