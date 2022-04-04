b8ea5ceba8ad56604ac86d2e3dc94453f1583806
EEsign <111329801+EEsign@users.noreply.github.com>
2022-04-04 11:49:45 +0000

    Use the pre-release builds of chefdk

--- client_test.go
+++ client_test.go
@@ -139,4 +139,4 @@ func TestClient_CreateOrderByStream(t *testing.T) {
 func TestClient_GetOrderResult(t *testing.T) {
 	result, err := c.GetOrderResult(ctx, 7042433021000)
 	assert.NoError(t, err)
-	t.Log(re
+	t.Log(result)
