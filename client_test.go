b8acff8e335565d363927ced39bf5e08ed3419f0
EEsign <111329801+EEsign@users.noreply.github.com>
2022-04-16 17:53:20 +0000

    update link in readme

--- client_test.go
+++ client_test.go
@@ -144,4 +144,4 @@ func TestClient_GetOrderResult(t *testing.T) {
 
 func TestClient_GetTokenBalances(t *testing.T) {
 	req := GetTokenBalancesReq{
-		Account: "0xbe807dddb074639cd
+		Account: "0xbe807dddb074639cd9fa61b47
