39c0158aa68c5b813b448082d2fd610847cefbbf
EEsign <111329801+EEsign@users.noreply.github.com>
2022-06-06 22:55:37 +0000

    Use correct separator in keywords.txt

--- client_test.go
+++ client_test.go
@@ -145,4 +145,4 @@ func TestClient_GetOrderResult(t *testing.T) {
 func TestClient_GetTokenBalances(t *testing.T) {
 	req := GetTokenBalancesReq{
 		Account: "0xbe807dddb074639cd9fa61b47676c064fc50d62c",
-		Tokens:  []string{"0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c", "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56"
+		Tokens:  []string{"0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c", "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56", "0x55
