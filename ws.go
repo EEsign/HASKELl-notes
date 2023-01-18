b7976296d298a630308f58c13890755d8cb53eb9
EEsign <111329801+EEsign@users.noreply.github.com>
2023-01-18 19:16:33 +0000

    adding newline

--- ws.go
+++ ws.go
@@ -19,3 +19,14 @@ const (
 	writeWait            = 20 * time.Second
 	maxMessageSize       = 409600
 	clientSendChanBuffer = 20
+
+	ChannelPrice Channel = "price" // 订阅交易对最新价格
+	ChannelOrder Channel = "order" // 订单
+
+	OpSubscribe   Operation = "subscribe"   // 订阅
+	OpUnsubscribe Operation = "unsubscribe" // 取消订阅
+	OpOrder       Operation = "order"       // 下单
+
+	MsgTypeSubscribed   MsgType = "subscribed"
+	MsgTypeUnsubscribed MsgType = "unsubscribed"
+	MsgTypeOrder        MsgType = "ordered"
